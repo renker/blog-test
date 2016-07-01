992fd5c3fb49c2a6a8c06461a4446f1251cfe292
renker <caishenchen@163.com>
2016-07-01 14:35:17 +0800

    1

---
+++ blog-test/src/main/java/com/ct/em/Status.java
@@ -0,0 +1,13 @@
+package com.ct.em;
+
+public enum Status {
+	SUCCESS("success"),ERROR("error"),WARN("warn");
+	
+	private String value;
+	Status(String value) {
+		this.value = value;
+	}
+	public String getValue() {
+		return value;
+	}
+}
